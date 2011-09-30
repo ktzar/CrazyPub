6c6de7cc8e037a7c9e6eae353b914a2f8260b605
kTzAR <miguelmail@gmail.com>
2011-09-30 17:47:03 +0100

    Menu for the game

---
+++ game/bartending.py
@@ -0,0 +1,186 @@
+import os, pygame, time
+import random
+from pygame.locals import *
+from sprites import *
+from stage import *
+import utils
+
+
+class Bartending():
+    def __init__(self, screen):
+        self.screen = screen
+        self.initialise()
+
+    def initialise(self):
+        """this function is called when the program starts.
+           it initializes everything it needs, then runs in
+           a loop until the function returns."""
+        #Initialize Everything
+
+        self.game_paused = False
+        #sounds
+        self.sounds = {};
+        #self.sounds['powerup'] = utils.load_sound('powerup.wav')
+        #self.sounds['music'].play()
+        #Create The Backgound
+        self.background, foo = utils.load_image('back.png')
+
+        #game variables
+        self.speed = 50 #the lower the faster
+        self.score = 0
+        self.mugs = 10
+        self.clients_served = 0
+        self.time = 0 #to check the stage for clients
+
+        #Display The Background
+        self.screen.blit(self.background, (0, 0))
+        pygame.display.flip()
+
+
+        self.level = Stage('level_1')
+        #The player's ship
+        self.bartender = Bartender()
+        #The dash indicators
+
+        #group that stores all enemies
+        self.beers       = pygame.sprite.Group()
+        #group that stores all powerups
+        self.clients     = pygame.sprite.Group()
+        #group for information sprites in the screen, should be rendered the last one
+        self.hud         = pygame.sprite.Group()
+        self.font = utils.load_font('saloon.ttf', 20)
+
+
+        self.game_started = False
+        self.game_finished = False
+        self.level_finished = False
+
+    def client_gone(self):
+        self.game_finished = True
+
+    def return_beer(self, client):
+        print "Return beer"
+        empty_beer = EmptyBeer(self, client.rect, client.cur_lane)
+        client.kill()
+        self.beers.add( empty_beer )
+
+
+    def emptybeer_arrived(self, emptybeer):
+        print "emptybeer_arrived"
+        #check if the bartender is in the same lane as the beer
+        emptybeer.kill()
+        if emptybeer.cur_lane != self.bartender.cur_lane :
+            if self.mugs < 1:
+                self.game_finished = True
+            else:
+                self.mugs -= 1
+        else:
+            self.score += 500
+
+    def break_beer(self):
+        if self.mugs < 1:
+            self.game_finished = True
+        else:
+            self.mugs -= 1
+
+    def handle_keys(self):
+        #Handle Input Events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                #exit
+                return
+            elif event.type == KEYDOWN and event.key == K_ESCAPE and self.game_finished == True:
+                pygame.quit()
+                quit()
+                
+            if event.type == KEYDOWN:
+                self.game_started = True
+                if event.key == K_ESCAPE:
+                    return False   #exit
+                elif event.key == K_SPACE:
+                    self.beers.add(Beer(self, self.bartender.rect, self.bartender.cur_lane))
+                    pass
+                elif event.key == K_UP:
+                    self.bartender.move_up()
+                elif event.key == K_DOWN:
+                    self.bartender.move_down()
+                elif event.key == K_p:
+                    self.game_paused = not self.game_paused
+
+        return True
+
+    #Main Loop
+    def loop(self):
+        #handle input events
+        esc_pressed = self.handle_keys()
+        if esc_pressed == False:
+            return
+
+        if self.game_started == False:
+            start_text = self.font.render('Press any key to start', 2, (255,255,255))
+            self.screen.blit(start_text, (150, 200))
+            pygame.display.flip()
+            return
+
+        if self.game_paused == 1:
+            start_text = self.font.render('Game paused', 2, (255,255,255))
+            self.screen.blit(start_text, (150, 200))
+            pygame.display.flip()
+            return
+
+        if self.time % self.speed == 0:
+            if self.time/self.speed > self.level.get_max_time():
+                self.game_finished = True
+                return
+            clients = self.level.get_clients_in_t(self.time/self.speed) #returns an array with 1 in the lanes with clients
+            for i in range(len(clients)):
+                if clients[i] == 1:
+                    self.clients.add(Client(self, lane=i))
+
+        for beer in self.beers:
+            if not isinstance(beer, Beer):
+                continue
+            clients_drinking  = pygame.sprite.spritecollide(beer, self.clients, False)
+            for client_drinking in clients_drinking:
+                if client_drinking.state == Client.WAITING:
+                    self.score += 100
+                    self.clients_served += 1
+                    beer.kill()
+                    client_drinking.start_drinking()
+                    #self.return_beer(client_drinking)
+                    break
+        try:
+            pass
+        except:
+            self.level_finished = True
+
+        #draw the level
+        all_sprites = pygame.sprite.Group()
+        all_sprites.add(self.beers.sprites())
+        all_sprites.add(self.clients.sprites())
+        all_sprites.add(self.bartender)
+        all_sprites.update()
+
+        #Move and draw the background
+        score_text = "Score: {0} Mugs: {1}".format(self.score, self.mugs)
+        text = self.font.render(score_text, 1, (255, 255, 255))
+        text_shadow = self.font.render(score_text, 1, (0,0,0))
+
+        self.screen.blit(self.background, (0, 0))
+        self.screen.blit(text_shadow, (12, 12))
+        self.screen.blit(text, (10, 10))
+
+        if self.game_finished == True:
+            gameover_text = self.font.render("Game Over", 2, (255, 255, 255))
+            self.screen.blit(gameover_text, (200, 200))
+            gameover_text = self.font.render("Press Esc", 2, (255, 255, 255))
+            self.screen.blit(gameover_text, (200, 230))
+        else:
+            all_sprites.draw(self.screen)
+        #draw all the groups of sprites
+        pygame.display.flip()
+        self.time += 1
+
+    #Game Over
+
+
---
+++ game/menu.py
@@ -0,0 +1,65 @@
+import pygame
+from pygame.locals import *
+import utils
+import math
+
+class Menu():
+    def __init__(self, screen):
+        self.font = utils.load_font('saloon.ttf', 20)
+        self.background, foo    = utils.load_image('back.png')
+        self.logo, foo          = utils.load_image('logo.png')
+        self.screen = screen
+        self.chosen_option = 0
+        self.options = [ "Start game", "Options", "Quit" ]
+        self.age = 0
+
+    def handle_keys(self):
+        #Handle Input Events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return False
+            elif event.type == KEYDOWN:                 
+                if event.key == K_UP:
+                    self.option_up()
+                elif event.key == K_DOWN:
+                    self.option_down()
+
+        return True
+
+    def option_down(self):
+        if self.chosen_option != len(self.options)-1:
+            self.chosen_option += 1
+
+    def option_up(self):
+        if self.chosen_option != 0:
+            self.chosen_option -= 1
+
+    def loop(self):
+        self.screen.blit(self.background, (0, 0))
+        self.screen.blit(self.logo, (10, 50))
+        self.age +=1
+
+        esc_pressed = self.handle_keys()
+        if esc_pressed == False:
+            return False
+
+
+        y = 200
+        i = 0
+        for option in self.options:
+            if i==self.chosen_option:
+                color = (255,100,100)
+                angle = math.sin(self.age/10)  * 15
+            else:
+                color = (255,255,255)    
+                angle = 0
+
+            text = pygame.transform.rotate(self.font.render(option, 2, (0,0,0,0)), 10)
+            self.screen.blit(text, (152, y+2))
+            text = pygame.transform.rotate(self.font.render(option, 2, color), 10)
+            self.screen.blit(text, (150, y))
+            y += 80
+            i += 1
+
+        pygame.display.flip()
+
