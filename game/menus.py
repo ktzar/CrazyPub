4d01547a22ae901987d149a344934517a3f8ec82
ktzar <miguelmail@gmail.com>
2011-10-09 14:38:17 +0100

    Add licensing

--- game/menus.py
+++ game/menus.py
@@ -1,3 +1,15 @@
+'''This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 import pygame
 from pygame.locals import *
 import utils
@@ -181,8 +193,7 @@ class Highscores(Abstract_Menu):
         self.events = pygame.event.get()
         for event in self.events:
             if event.type == KEYDOWN:                 
-                if event.key == K_ESCAPE:
-                    self.finished = True
+                self.finished = True
         return True
 
 class Newhighscore(Abstract_Menu):
@@ -224,6 +235,7 @@ class Newhighscore(Abstract_Menu):
                 if event.key == K_RETURN:
                     #TODO store highscore
                     highscores = Highscores_data()
+                    print "Save new highscore {0} : {1}".format(self.player_name, self.bartending.score)
                     highscores.set_newhighscore(self.player_name, self.bartending.score)
                     self.finished = True
                     return
--- game/stage.py
+++ game/stage.py
@@ -1,3 +1,16 @@
+'''This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
+
 import random
 import pygame
 import utils
@@ -24,7 +37,6 @@ class Stage():
 
 
     def get_clients_in_t(self, time):
-        print "{0} of {1}".format(time, len(self.level_data))
         if time >= len(self.level_data) :
             return False
         else:
